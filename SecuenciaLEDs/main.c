9b9b311ff663c5ff47931d5516056c67ca1994df
vero2405 <veronica.calderon@ucuenca.edu.ec>
2024-05-05 15:50:24 -0500

    Updated

--- SecuenciaLEDs/main.c
+++ SecuenciaLEDs/main.c
@@ -1,4 +1,5 @@
-#include "mbed.h"
+//#include "mbed.h"
+//#include <stdint.h>
 #include <stdint.h>
 
 const int16_t LED_GREEN = 7;
@@ -11,23 +12,19 @@ const int16_t SW3 = 11;
 const int16_t SW4 = 12;
 
 
-int16_t leds[] = {LED_GREEN, LED_YELLOW, LED_RED, LED_BLUE};
-int16_t len = sizeof(leds)/sizeof(int16_t);
-enum dir {ascending, descending};
-typedef struct{
-  int16_t* leds;
-  int16_t len;
-  dir orientation;
-} control_leds;
+//int16_t leds[] = {LED_GREEN, LED_YELLOW, LED_RED, LED_BLUE};
+//int16_t len = sizeof(leds)/sizeof(int16_t);
+//enum dir {ascending, descending};
+//typedef struct{
+  //int16_t* leds;
+  //int16_t len;
+  //dir orientation;
+//} control_leds;
 
-void turnOffLeds(control_Leds cleds);
-dir readKey(int16_t key);
-void ledSequence(int16_t* leds, int16_t len, dir orientation);
+//void turnOffLeds(control_Leds cleds);
+//dir readKey(int16_t key);
+//void ledSequence(int16_t* leds, int16_t len, dir orientation);
 
-void setup() {
-  // put your setup code here, to run once:
-
-}
 
 void loop() {
   // put your main code here, to run repeatedly:
