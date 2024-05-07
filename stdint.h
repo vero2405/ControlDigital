f6e1ff3c01423de0ce1424e9d26c4d7098cd12a4
vero2405 <veronica.calderon@ucuenca.edu.ec>
2024-05-07 00:37:09 -0500

    Actualizacion

---
+++ stdint.h
@@ -0,0 +1,30 @@
+#ifndef _STDINT_H
+#define _STDINT_H
+
+// Definiciones de tipos de datos enteros de ancho fijo
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef short int16_t;
+typedef unsigned short uint16_t;
+typedef int int32_t;
+typedef unsigned int uint32_t;
+typedef long long int64_t;
+typedef unsigned long long uint64_t;
+
+// Macros para definir constantes de ancho fijo
+#define INT8_MIN    (-128)
+#define INT16_MIN   (-32768)
+#define INT32_MIN   (-2147483648)
+#define INT64_MIN   (-9223372036854775807LL - 1)
+
+#define INT8_MAX    (127)
+#define INT16_MAX   (32767)
+#define INT32_MAX   (2147483647)
+#define INT64_MAX   (9223372036854775807LL)
+
+#define UINT8_MAX   (255)
+#define UINT16_MAX  (65535)
+#define UINT32_MAX  (4294967295U)
+#define UINT64_MAX  (18446744073709551615ULL)
+
+#endif /* _STDINT_H */
