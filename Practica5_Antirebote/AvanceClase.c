6c1698597b43432c6f5f8f8d5b63406326945767
vero2405 <veronica.calderon@ucuenca.edu.ec>
2024-06-21 12:30:00 -0500

    Nueva actualizacion para detectar cambio de estado del SW en la funcion del timer

--- Practica5_Antirebote/AvanceClase.c
+++ Practica5_Antirebote/AvanceClase.c
@@ -19,7 +19,7 @@ void inicializarMEF(){
 }
 
 void actualizarMEF(){
-    int val = digitalRead(SW1);
+  int val = digitalRead(SW1);
   static int prevVal = val;
   bool cambio = val ^ prevVal;  // (^: XOR)
 
@@ -29,35 +29,29 @@ void actualizarMEF(){
         // Inicializar Hardware
         Serial.println("estado Inicial");
         pinMode(SW1,INPUT_PULLUP);
-        estadoActual = ESTADO_UP;  // Condicion de cambio (de momento)
+        
+        if (val==1){
+          estadoActual = ESTADO_UP;
+        } else{
+          estadoActual = ESTADO_DOWN;
+        }
         break;
+        
     case ESTADO_UP:
         Serial.println("estado UP");
         estadoActual = ESTADO_FALLING;
-        break;
-    case ESTADO_FALLING:
-        Serial.println("estado FALLING");
-        //TIMER 40 MS
-        if(){
-            estadoActual = ESTADO_UP;
-        }
-        else{
-            estadoActual = ESTADO_DOWN;
-        }
         break;
     case ESTADO_DOWN:
         Serial.println("estado DOWN");
         estadoActual = ESTADO_RISING;
         break;
+    case ESTADO_FALLING:
+        Serial.println("estado FALLING");
+        DetectorDurante40ms();
+        break;
     case ESTADO_RISING:
         Serial.println("estado FALLING");
-        //TIMER 40 MS
-        if(){
-            estadoActual = ESTADO_UP;
-        }
-        else{
-            estadoActual = ESTADO_DOWN;
-        }
+        DetectorDurante40ms();
         break;
     default:
         inicializarMEF();
@@ -67,23 +61,30 @@ void actualizarMEF(){
     prevVal = val;
 }
 
+
 void setup(){
     inicializarMEF();
-    Serial.begin(11500);
+    Serial.begin(115200);
 }
 void loop(){
     actualizarMEF();
 }
 
+
 volatile uint16_t previousMillis = 0;  // Variable para almacenar el tiempo anterior
 volatile uint16_t currentMillis = millis();  // Obtener el tiempo actual en milisegundos
 
 void DetectorDurante40ms(){
-    if (currentMillis - previousMillis >= 40) {
-        previousMillis = currentMillis;  // Actualizar el tiempo anterior
-        // estado del pin
-        }
-    // activamos el flag
+  if (currentMillis - previousMillis >= 40) {
+    previousMillis = currentMillis;  // Actualizar el tiempo anterior    
+  }
+ // En los 40 ms detecta el flag
+  int val = digitalRead(SW1);
+   if(val==1){  // activamos el flag
+      estadoActual = ESTADO_UP;
+  }
+  else{
+      estadoActual = ESTADO_DOWN;
+  }
 
-    }
 }
