256f4d99a343cd1bfd8d1049dd1cfee30006b7b4
vero2405 <veronica.calderon@ucuenca.edu.ec>
2024-06-26 17:19:16 -0500

    Archivo .h para definicion de LEDs y SWs

---
+++ Practica4_Leds/inc/hal.h
@@ -0,0 +1,18 @@
+#ifndef __HAL_H
+#define __HAL_H_
+
+#include <stdint.h>
+#include <Arduino.h>
+
+#define LED_GREEN 7
+#define LED_YELLOW 6
+#define LED_RED 5
+#define LED_BLUE 4
+
+
+#define SW1 2
+#define SW2 3
+#define SW3 11
+#define SW4 12
+
+#endif
