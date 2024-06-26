53346a166577f59b651ef539da202a0ebae95b50
vero2405 <veronica.calderon@ucuenca.edu.ec>
2024-06-26 17:22:35 -0500

    Archivos .c y .h para los pulsantes

---
+++ Practica4_Leds/inc/pulsantes.h
@@ -0,0 +1,42 @@
+#ifndef __PULSANTES_H
+#define __PULSANTES_H
+
+#include <stdint.h>
+#include "hal.h"
+
+
+// Estructura para representar los pulsadores
+typedef struct {
+    uint8_t SW;
+    bool activacion;
+} control_pulsadores;
+
+control_pulsadores pulsadores[] = {
+    {SW1, false}, // SW1
+    {SW2, false}, // SW2
+    {SW3, false}, // SW3
+    {SW4, false} // SW4
+};
+
+// Para antirebote mediante Maquina de Estado
+
+typedef enum {
+    ESTADO_INICIAL,
+    ESTADO_UP,
+    ESTADO_FALLING,
+    ESTADO_DOWN,
+    ESTADO_RISING
+} estado_t;
+
+typedef struct {
+    int pin;
+    estado_t estadoActual;
+    unsigned long previousMillis;
+} switch_t;
+
+bool leeSW(int16_t sw);
+void inicializarMEF(switch_t* sw);
+void DetectorDurante40ms(switch_t* sw);
+void actualizarMEF(switch_t* sw);
+
+#endif
