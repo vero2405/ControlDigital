08e477c17f4afe8be07cbe6b4712a80c78e85d54
vero2405 <veronica.calderon@ucuenca.ecu.ec>
2024-06-29 20:41:33 -0500

    Borrado de carpeta inecesarias usadas a lo largo del trabajo

--- Practica5_Antirebote/pulsantes.c
+++
@@ -1,7 +0,0 @@
-#include "pulsantes.h"
-#include <Arduino.h>
-
-int leerTecha(int16_t tecla){
-    int val = !digitalRead(tecla);
-    return val;
-}
--- Practica5_Antirebote/pulsantes.h
+++
@@ -1,9 +0,0 @@
-#ifndef PULSANTES_H
-#define __PULSANTES_H
-
-#include <stdint.h>
-#include "hal.h"
-
-int leerTecla(int16_t tecla);
-
-#endif
